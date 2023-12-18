df4850b6cbab3d3c0f2553f1d2f5950f07544e41
Christopher Hofmann <cwh@suse.de>
2023-12-18 14:23:16 +0100

    Use BS carousel for displaying the whole week (#6)
    * Carousel, first try
    
    * Put the whole week into a bs carousel
    
    * Add date; better formatting

--- web/mahlzeit.js
+++ web/mahlzeit.js
@@ -14,25 +14,53 @@ if ("serviceWorker" in navigator) {
   }
 }
 
-const dayNames = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'];
-var today = new XDate().addDays(0);
+const mzCarouselElement = document.querySelector('#mzCarousel')
+const carousel = new bootstrap.Carousel(mzCarouselElement, {
+  touch: true
+});
+
+const dayNames = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+var today = new XDate();
 if (today.getHours() >= 13) { today.addDays(1) };
-var weekday = dayNames[today.getDay()];
-var weekString = today.toString("yyyy'W'ww");
 
-$(document).ready(function(){
-  $( "#headline").text(weekday);
+$(document).ready(function() {
+  if (today.getDay() == 0 || today.getDay() == 6) { today.setWeek(today.getWeek() + 1) };
+  var template = $("#mzCarousel .carousel-item.active").detach();
+
+  var ci = renderDay(today, template.clone());
+  ci.appendTo("#mzCarousel .carousel-inner");
+
+  while (today.getDay() < 5) {
+    ci = renderDay(today.addDays(1), template.clone()).removeClass("active");
+    ci.appendTo("#mzCarousel .carousel-inner");
+  }
+});
+
+function tagPrice(elem) {
+  const regex = /\b([0-9,]+&nbsp;€)/u;
+  return elem.html(elem.html().replace(regex, "<span class='price'>$&</span>"));
+}
+
+function renderDay(date, template) {
+  var weekday = dayNames[date.getDay()];
+  var weekString = date.toString("yyyy'W'ww");
+
+  template.find(".mz-day").text(weekday);
+  template.find(".mz-date").text(date.toLocaleDateString());
   $.getJSON( "mahlzeit.json", function( data ) {
-    var p = $( "#today .mz-dish" );
+    var p = template.find( ".mz-menu .mz-dish" ).clone();
 
-    if ( typeof data[weekString][weekday] !== 'undefined' ) {
-      p.remove();
-    }
+    if ( typeof data[weekString] === 'undefined' || typeof data[weekString][weekday] === 'undefined' ) { return template; }
+
+    template.find(".mz-menu").empty();
 
     $.each( data[weekString][weekday], function( key, val ) {
-      p.clone().text(val).appendTo("#today");
+      item = tagPrice(p.clone().text(val))
+      item.appendTo(template.find(".mz-menu"));
     });
   }).fail(function(){
-    $( "#headline").text("Oops! Could not load the menu!");
+    template.find(".mz-day").text("Oops! Could not load the menu!");
   });
-});
+
+  return template;
+}
