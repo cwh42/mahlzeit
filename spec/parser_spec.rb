0197ea5b2a846ff2cea4eb093d4748d5170240f7
Christopher Hofmann <cwh@suse.de>
2026-02-17 15:58:39 +0100

    Add spec for v2

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -44,4 +44,35 @@ RSpec.describe do
       end
     end
   end
+
+  describe '#v2ify' do
+    subject { v2ify(*parse(text)) }
+
+    it 'returns an Array' do
+      is_expected.to be_an Array
+    end
+
+    it 'returns an Array having 5 elements' do
+      expect(subject.size).to eq(5)
+    end
+
+    it 'returns an Array having all elements be Hashes' do
+      is_expected.to all(be_a(Hash))
+    end
+
+    it 'returns an Array having all elements be Hashes having a Date' do
+      dates = subject.map { |h| h[:date] }
+      expect(dates).to all(match(/2\d{3}-\d{2}-\d{2}/))
+    end
+
+    it 'returns an Array having all elements be Hashes having a Menu' do
+      menues = subject.map { |h| h[:menu] }
+      expect(menues).to all(be_an(Array))
+    end
+
+    it 'returns an Array having all elements be Hashes having a Menu with some entries' do
+      menue_sizes = subject.map { |h| h[:menu].size }
+      expect(menue_sizes).to all(be_within(1).of(6))
+    end
+  end
 end
