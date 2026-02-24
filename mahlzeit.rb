9c7d42ac49ad2c916717a01ce573c9ca5628b4a1
Christopher Hofmann <cwh@suse.de>
2026-02-24 18:18:51 +0100

    Improved JSON updating for v2; make vc2 the default output format

--- mahlzeit.rb
+++ mahlzeit.rb
@@ -15,7 +15,7 @@ require './lib/helpers'
 json_file = nil
 cleanup = false
 lint = false
-v2 = false
+v1 = false
 
 OptionParser.new do |parser|
   parser.banner = "Usage: #{$PROGRAM_NAME} [-d]"
@@ -23,7 +23,7 @@ OptionParser.new do |parser|
   parser.on('-u', '--update FILE', 'Update <FILE>') { |f| json_file = f }
   parser.on('-c', '--cleanup', 'Filter past weeks') { |c| cleanup = c }
   parser.on('-l', '--lint', 'Fix some common typos and style issues') { |l| lint = l }
-  parser.on('-2', '--v2', 'Use new file format') { |v| v2 = v }
+  parser.on('-1', '--v1', 'Use old file format') { |v| v1 = v }
 end.parse!
 
 filenames = ARGV.select { |param| param.downcase.include?('.pdf') }
@@ -42,21 +42,34 @@ if @debug
 end
 
 output = {}
-out_arr = []
+output_v2 = {}
 
 if json_file
   begin
     File.open json_file do |f|
       data = JSON.parse f.read #, { symbolize_names: true }
 
-      warn "#{json_file} contains an #{data.class}" if @debug
+      warn "#{json_file} contains a(n) #{data.class}" if @debug
 
       case data.class.to_s
       when "Hash"  # v1
         output = data
+
+        data.each do |date, menu|
+          v2data = v2ify(Date.parse(date), menu)
+          v2data.each do |d|
+            output_v2[d[:date]] = d
+          end
+        end
+
         out_arr.concat(*data.map { |date, menu| v2ify(Date.parse(date), menu) })
       when "Array" # v2
-        out_arr = data.map { |d| d.transform_keys(&:to_sym)}
+        if v1
+          warn "Not able to convert v2 data to v1."
+          exit
+        end
+
+        output_v2 = data.to_h { |d| [d['date'], d.transform_keys(&:to_sym)] }
       else
         warn "JSON file seems not to be the right format."
         exit
@@ -74,7 +87,12 @@ filenames.each do |filename|
       menu = lint(menu) if lint
       # add the week to the output as a hash with ISO 8601 week date, like "2022W40", as a key
       output[date.strftime('%GW%V')] = menu
-      out_arr.concat(v2ify(date, menu)) if v2
+
+      v2data = v2ify(date, menu)
+      v2data.each do |date|
+        output_v2[date[:date]] = date
+      end
+
       warn "-- new week --#{'-' * 60}" if @debug
     rescue RuntimeError => e
       warn "Skip #{filename} due to #{e.message}"
@@ -85,11 +103,11 @@ end
 # Delete past data, if desired
 if cleanup
   output.delete_if { |w| past? w }
-  out_arr.delete_if { |d| Date.parse(d[:date]) < Date.today }
+  output_v2.delete_if { |d| Date.parse(d) < Date.today }
 end
 
 # Print json sorted by keys
-json_output = JSON.pretty_generate( v2 ? out_arr.sort { |a, b| a[:date] <=> b[:date] }.uniq { |d| d[:date] } : Hash[*output.sort.flatten])
+json_output = JSON.pretty_generate( v1 ? Hash[*output.sort.flatten] : output_v2.values.sort { |a, b| a[:date] <=> b[:date] })
 
 if json_file
   File.open json_file, 'w' do |f|
