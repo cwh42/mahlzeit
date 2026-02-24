c317641137b461b32db7ec1ba04bf9ed3de01ef9
Christopher Hofmann <cwh@suse.de>
2026-02-24 18:54:00 +0100

    Update tests; add test for v2 output

--- spec/mahlzeit_spec.rb
+++ spec/mahlzeit_spec.rb
@@ -12,39 +12,75 @@ RSpec.describe "mahlzeit.rb" do
 
   context "with input file" do
     subject(:json) do
-      cmd = %(./mahlzeit.rb #{Dir.pwd}/spec/pdf/16.02.2026*.pdf)
+      cmd = %(./mahlzeit.rb #{v1 ? '-1' : ''} #{Dir.pwd}/spec/pdf/16.02.2026*.pdf)
       JSON.parse(`#{cmd}`)
     end
 
-    it 'outputs proper JSON' do
-      expect(json).to be_a Hash
+    let(:v1) { false }
+
+    context 'default output format (v2)' do
+      it 'outputs proper JSON' do
+        expect(json).to be_an Array
+      end
+
+      context 'the returned menu' do
+        subject(:menu) { json.first }
+
+        it 'has a date' do
+          expect(menu['date']).to a_string_matching(/2\d{3}-\d{2}-\d{2}/)
+        end
+
+        context 'the returned daily menu' do
+          subject(:daily_menu) { menu.fetch('menu') }
+
+          it 'is an Array' do
+            expect(daily_menu).to be_an Array
+          end
+
+          it 'has 4 elements' do
+            expect(daily_menu.size).to eq(4)
+          end
+
+          it 'has all of its elements be Strings' do
+            expect(daily_menu).to all(be_a(String))
+          end
+        end
+      end
     end
 
-    it 'all keys are a ISO 8601 week date' do
-      expect(json.keys).to all(a_string_matching(/2\d{3}W\d{2}/))
-    end
+    context 'old output format' do
+      let(:v1) { true }
 
-    context 'the returned menu' do
-      subject(:menu) { json.values.first }
-
-      it 'has 5 elements' do
-        expect(menu.size).to eq(5)
+      it 'outputs proper JSON' do
+        expect(json).to be_a Hash
       end
 
-      it 'has all of its keys be Strings' do
-        expect(menu.keys).to all(be_a(String))
+      it 'all keys are a ISO 8601 week date' do
+        expect(json.keys).to all(a_string_matching(/2\d{3}W\d{2}/))
       end
 
-      it 'has all of its values be Hashes' do
-        expect(menu.values).to all(be_a(Hash))
-      end
+      context 'the returned menu' do
+        subject(:menu) { json.values.first }
 
-      context 'the returned daily menu' do
-        subject(:daily_menu) { menu.values.first }
+        it 'has 5 elements' do
+          expect(menu.size).to eq(5)
+        end
 
-        it 'has all of its keys and values be Strings' do
-          expect(daily_menu.keys).to all(be_a(String))
-          expect(daily_menu.values).to all(be_a(String))
+        it 'has all of its keys be Strings' do
+          expect(menu.keys).to all(be_a(String))
+        end
+
+        it 'has all of its values be Hashes' do
+          expect(menu.values).to all(be_a(Hash))
+        end
+
+        context 'the returned daily menu' do
+          subject(:daily_menu) { menu.values.first }
+
+          it 'has all of its keys and values be Strings' do
+            expect(daily_menu.keys).to all(be_a(String))
+            expect(daily_menu.values).to all(be_a(String))
+          end
         end
       end
     end
